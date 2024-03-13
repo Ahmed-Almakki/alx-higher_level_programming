e39c87c17efac19203cde59faa7d54637bdb9911
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-03-12 17:01:02 -0700

    hurry

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+for i in range(10):
+    for j in range(10):
+        if j > i:
+            print("{}{}".format(i, j), end="")
+            if i == 9 and  == 8:
+                break
+            else:
+                print(",", end="")
+
