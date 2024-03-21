3cac38f164a53efa2399c84ef4c6fb04736aa913
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-03-20 18:29:38 -0700

    empty and for

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
     sm, den = 0, 0
+    if len(my_list) == 0:
+        return 0
     for i in my_list:
         sm += i[0] * i[1]
         den += i[1]
