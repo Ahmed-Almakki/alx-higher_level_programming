2794b88ff5cf18fcf12985f2bb5103731c6a3e86
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-03-20 19:38:05 -0700

    roman string

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    um = 0
+    rom = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    if len(roman_string) == 2:
+        if rom[roman_string[1]] > rom[roman_string[0]]:
+            return rom[roman_string[1]] - rom[roman_string[0]]
+        else:
+            return rom[roman_string[0]] + rom[roman_string[1]]
+    for i in range(len(roman_string)):
+        um += rom[roman_string[i]]
+    return um
