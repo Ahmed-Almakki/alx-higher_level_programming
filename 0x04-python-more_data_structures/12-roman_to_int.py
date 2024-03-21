a5de7aaf6b5a17bc2b651b612af9dddcc5b74176
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-03-20 19:47:01 -0700

    check unknown string

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,6 +4,9 @@ def roman_to_int(roman_string):
     rom = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     if type(roman_string) != str or roman_string is None:
         return 0
+    for i in roman_string:
+        if i not in rim.keys():
+            return 0
     if len(roman_string) == 2:
         if rom[roman_string[1]] > rom[roman_string[0]]:
             return rom[roman_string[1]] - rom[roman_string[0]]
