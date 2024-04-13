ad26ecd1fa04a3e50255b84ec364059f19d1113e
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-04-13 13:00:41 -0700

    try inside for

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,10 +6,10 @@ def safe_print_list(my_list=[], x=0):
         return 0
     for j in my_list:
         count += 1
-    try:
-        for i in range(x):
+    for i in range(x):
+        try:
             print(my_list[i], end="")
-        print()
-    except IndexError:
-        pass
+        except IndexError:
+            pass
+    print()
     return i + 1
