e86ae300b5afaa1cf37880d947be4b8003707f42
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-04-13 13:14:02 -0700

    check for x and list and number of element

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     for j in my_list:
         count += 1
-    if (count == 0) or (x == 0) or (x > count):
+    if (count == 0) or (x == 0):
         return 0
     for i in range(x):
         try:
