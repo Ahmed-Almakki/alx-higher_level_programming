be9fa143ab835acf316ca5638dd91d61ba73fc53
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-04-13 12:51:07 -0700

    check x = 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
+    count = 0
+    if x == 0:
+        return 0
+    for j in my_list:
+        count += 1
     try:
         for i in range(x):
             print(my_list[i], end="")
