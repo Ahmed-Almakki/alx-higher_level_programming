b7d02b11a2de46d1048db9fe1a68d4ddb6b83fc5
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-04-13 13:12:35 -0700

    check for x and list and number of element

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,8 +8,8 @@ def safe_print_list(my_list=[], x=0):
         return 0
     for i in range(x):
         try:
-            print(my_list[i], end="")
+            print("{}".format(my_list[i]), end="")
         except IndexError:
-            pass
+            break
     print()
     return i + 1
