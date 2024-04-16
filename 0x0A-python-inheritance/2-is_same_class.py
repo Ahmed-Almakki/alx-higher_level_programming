25d53e767c736f1ebfb900c068ca38e478b01b5f
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-04-15 18:54:56 -0700

    j

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,9 +3,7 @@
 
 
 def is_same_class(obj, a_class):
-    """
-    checking the type
-    """
-    if (type(obj) == a_class):
+    """checj for class"""
+    if type(obj) == a_class:
         return True
     return False
