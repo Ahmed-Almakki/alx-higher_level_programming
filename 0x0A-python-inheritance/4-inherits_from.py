4907f9401516bc7abe05e5169fa8894644fa3352
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-04-17 16:02:44 -0700

    is

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,6 +6,6 @@ def inherits_from(obj, a_class):
     """
     checjing the type
     """
-    if (type(obj) != a_class):
+    if (type(obj) is not a_class):
         return issubclass(type(obj), a_class)
     return False
