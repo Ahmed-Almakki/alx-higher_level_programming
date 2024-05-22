90596dcc28717ef4031a96702519c7fb443e9a99
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-05-22 13:02:15 -0700

    create table with foriegn key

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,6 @@
+-- script that creates the database hbtn_0d_usa and the table cities
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	state_id INT, FOREIGN KEY(state_id) REFERENCES states(id),
+	name VARCHAR(256) NOT NULL
+	);
