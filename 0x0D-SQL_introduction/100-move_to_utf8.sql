1f9fff04812142d07ef9a25c262f3bb245becb5e
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-05-21 19:49:54 -0700

    a

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,4 @@
 -- script that converts hbtn_0c_0 database to UTF8
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE hbtn_0c_0.second_table CONVERT TO CHARACTER SET utf8mb4;
-ALTER TABLE hbtn_0c_0.second_table MODIFY name TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
+ALTER TABLE hbtn_0c_0.second_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
+ALTER TABLE hbtn_0c_0.second_table MODIFY name TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
