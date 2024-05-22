082fdc760c9e4ece96562bbbc74cf8005b845fc1
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-05-22 15:22:37 -0700

    alisas for colmn name

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- script that lists all cities contained in the database hbtn_0d_usa.
-SELECT cities.id AS "id", cities.name AS "name", states.name
+SELECT cities.id AS "id", cities.name AS "name", states.name AS "name"
 FROM cities
 LEFT JOIN states
 ON cities.id = states.id
