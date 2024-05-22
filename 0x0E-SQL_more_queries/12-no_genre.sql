db6a1ee824912b7ea154c5a83d0e1b4c2328618d
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-05-22 15:30:07 -0700

    condition to make sure is null

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,6 @@
+-- join type
+SELECT tv_shows.title AS "title", tv_show_genres.genre_id AS "genre_id"
+FROM tv_shows  LEFT OUTER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+WHERE genre_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id
