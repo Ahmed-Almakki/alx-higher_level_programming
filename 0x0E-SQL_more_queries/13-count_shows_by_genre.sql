3664adf371f2b7172555ef679377ffe92517be2d
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-05-23 07:53:44 -0700

    inner join

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- script that lists all genres from hbtn_0d_tvshows and displays the
+SELECT name AS "genre", COUNT(genre_id) AS "number_of_shows"
+FROM tv_show_genres
+INNER JOIN tv_genres
+ON tv_show_genres.show_id = tv_genres.id
+GROUP BY genre
+ORDER BY number_of_shows DESC
