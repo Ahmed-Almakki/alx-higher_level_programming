4d8f1e0850af7f45f5925dc93102b5d03540b0b3
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-05-23 08:00:44 -0700

    On clause between generid and id

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,6 +2,6 @@
 SELECT name AS "genre", COUNT(genre_id) AS "number_of_shows"
 FROM tv_show_genres
 INNER JOIN tv_genres
-ON tv_show_genres.show_id = tv_genres.id
+ON tv_show_genres.genre_id = tv_genres.id
 GROUP BY genre
 ORDER BY number_of_shows DESC
