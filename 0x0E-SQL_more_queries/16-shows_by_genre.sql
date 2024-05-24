943641e1b1d3f2f405581382815796ee90741c3c
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-05-23 18:22:01 -0700

    left left join

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
+SELECT title, name
+FROM tv_shows as tv
+LEFT JOIN
+	tv_show_genres as tv_g ON tv.id = tv_g.show_id
+LEFT JOIN
+	tv_genres as gn ON gn.id = tv_g.genre_id
+ORDER BY title, name
