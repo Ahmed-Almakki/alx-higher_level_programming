a040afb479aab150b8db2e9e783974b3eec87137
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-06-20 23:55:39 +0200

    used sort method

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,4 +4,12 @@ if (process.argv[2] == null) {
 } else if (process.argv.length === 3) {
   console.log(0);
 } else {
-  
+  let list = [];
+  let i;
+  for (i = 2; i < process.argv.length; i++) {
+    list[i - 2] = Math.trunc(process.argv[i]);
+  }
+  list = list.sort();
+  const result = list.length - 2;
+  console.log(list[result]);
+}
