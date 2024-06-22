de62bb1648556b95ec4a3ff32189122e7904eb37
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-06-22 04:36:38 -0700

    first commit

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""rite a script that takes in an argument and displays all values"""
+import MySQLdb as mysql
+import sys
+
+
+if __name__ == "__main__":
+    argv = sys.argv
+    db = mysql.connect(host="localhost", port=3306, user=argv[1],
+                       password=argv[2],
+                       database=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    rows = cur.fetchmany(2)
+    for row in rows:
+        print(row)
