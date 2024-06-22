5037fe07486b9e12b85acec05c9d26d2ff00f915
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-06-21 17:05:12 -0700

    tab for if __name__

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
     db = mysql.connect(host="localhost", user=sys.argv[1],
                        password=sys.argv[2], database=sys.argv[3], port=3306)
 
-cur = db.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-rows = cur.fetchall()
-for row in rows:
-    print(row)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
