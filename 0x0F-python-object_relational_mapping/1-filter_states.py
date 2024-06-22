569908aae371b3709781b4884adb0089fccb5472
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-06-22 07:26:58 -0700

    check inside python not sql

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,8 @@ if __name__ == "__main__":
                        password=argv[2],
                        database=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
-        print(row)
+        if row[1][0] == "N":
+            print(row)
