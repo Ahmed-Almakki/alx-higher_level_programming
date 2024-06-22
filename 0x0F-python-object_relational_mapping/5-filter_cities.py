eb4ab285e2ea4b69c84256d3c900a26f4a2a64bb
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-06-22 09:26:46 -0700

    first commit

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Write a script that takes in the name of a state as an argument"""
+import MySQLdb as mysql
+import sys
+
+
+if __name__ == "__main__":
+    argv = sys.argv
+    db = mysql.connect(host="localhost", port=3306,
+                       user=argv[1],
+                       password=argv[2],
+                       database=argv[3])
+    c = db.cursor()
+    qury = "SELECT cities.name \
+            FROM cities INNER JOIN states \
+            ON states.id = cities.state_id \
+            WHERE states.name = %s \
+            ORDER BY cities.id ASC"
+    c.execute(qury, (argv[4],))
+    rows = c.fetchall()
+    i = 1
+    for row in rows:
+        print(row[0], end="")
+        if i != len(rows):
+            print(",", end=" ")
+        else:
+            print()
+        i += 1
