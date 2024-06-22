34b379e1f99a58780dd28f03f0758f7e487329e7
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-06-22 05:08:07 -0700

    first commit

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Write a script that takes in an argument and displays all values"""
+import MySQLdb as mysql
+import sys
+
+
+if __name__ == "__main__":
+    argv = sys.argv
+    db = mysql.connect(host="localhost", port=3306, 
+                       user=argv[1],
+                       password=argv[2],
+                       database=argv[3])
+    cur = db.cursor()
+    query ="SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(argv[4])
+    cur.execute(query)
+    rows = cur.fetchone()
+    while rows is not None:
+        print(rows)
+        rows = cur.fetchone()
