6a4b3b13f2c0550e7ebbb621d5fbb602ee52fe9c
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-07-09 18:08:43 -0700

    first commit

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" script that takes in a letter and sends a POST"""
+import sys
+import requests
+
+
+if __name__ == "__main__": 
+    if len(sys.argv) == 1:
+        q = ""
+    else:
+        q = sys.argv[1]
+    parm = {'q': q}
+    x = requests.post("http://0.0.0.0:5000/search_user", data=parm) 
+    js = x.json()
+    if js == {}:
+        print("No result")
+    else:
+        print("[{}] {}".format(js.get("id"), js.get("name")))
