0ea14370f06a3c5b4c816f09338b2d689ab11615
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-07-30 14:22:03 -0700

    semistandard just like pycodstyle

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-const fs = require("node:fs");
+const fs = require('node:fs');
 
-fs.readFile(process.argv[2], "utf-8", (error, data) => {
-	if (error) {
-		console.log(error);
-	} else {
-		console.log(data);
-	}
+fs.readFile(process.argv[2], 'utf-8', (error, data) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
 });
